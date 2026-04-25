a85b71b15f308b76d7c2a8324b14609686b07117
mehadishisir <mehadishisir@gmail.com>
2026-04-26 00:00:47 +0600

    try map

--- basicArray.js
+++ basicArray.js
@@ -7,8 +7,16 @@ for(i=0;i<=5000;i++){
     }
     secoundArray.push(i)
 }
-console.time("firstArray.length")
-console.timeEnd("firstArray.length")
+console.log(firstArray.length)
+console.log(secoundArray.length)
+console.time("map1")
+const firstUser=firstArray.map((num)=>{
+    return num*2
+})
+console.timeEnd("map1")
+console.time("map2")
+const secondUser=secoundArray.map((num)=>{
+    return num*2
+})
+console.timeEnd("map2")
 
-console.time("secoundArray.length")
-console.timeEnd("secoundArray.length")
