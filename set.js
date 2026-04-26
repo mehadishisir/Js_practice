ebd6c9da9ae01065d83ef144905cbca28992ee7b
mehadishisir <mehadishisir@gmail.com>
2026-04-26 12:14:59 +0600

    add data using set to array

--- set.js
+++ set.js
@@ -1,2 +1,12 @@
-const set = new Set();
-console.log(set)
+// const set = new Set();
+// set.add(1);
+// set.add(2);
+// set.add(3);
+// console.log(set)
+// set.add(4)
+const arr = ["banana","apple","orange"]
+const set = new Set(arr)
+set.add("grape")
+const set2 =Array.from(set)
+set.add("grape")
+console.log(set2)
