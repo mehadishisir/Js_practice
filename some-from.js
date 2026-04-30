6e101f445722710abdd97d5de459c67144a2b8f2
mehadishisir <mehadishisir@gmail.com>
2026-04-30 23:06:13 +0600

    explore array.form

--- some-from.js
+++ some-from.js
@@ -2,7 +2,7 @@
 
 const numbers = [1, 2, 3, 7, 5];
 const hasEvenNumber = numbers.some(num=>num%2 === 0)
-console.log("Has Even Number:", hasEvenNumber);
+// console.log("Has Even Number:", hasEvenNumber);
 
 
 
@@ -12,4 +12,9 @@ const featureAccessRoles = ["admin", "manager"];
 
 
 const hasAccess = currentUserRoles.some(role=> featureAccessRoles.includes(role))
-console.log("Has Access:", hasAccess);
+// console.log("Has Access:", hasAccess);
+
+// array.from
+
+const range = (start,stop,step)=>Array.from({length:Math.ceil((stop-start)/step)},(_,i)=>start+i*step)
+console.log("Range from 0 to 10 with step 2:", range(1,10,2));
